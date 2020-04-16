7165433f8b4c03893364b2b376d723a68c8780a1
DennisVargas <dvargas.software@gmail.com>
2020-04-16 14:03:09 -0700

    Creates a scraper for indeed jobs in vanouver and longview

--- indeed-scraper.js
+++ indeed-scraper.js
@@ -1,46 +1,27 @@
-const puppeteer = require('puppeteer');
-const $ = require('cheerio');
-// const fs = require('fs');
 const fi = require('./utils/file-interface');
+const pptr = require('./utils/puppeteer-interface');
 const indeedParse = require('./indeed-parser');
 
-const url = "https://www.indeed.com/jobs?q=software+developer&l=vancouver%2C+WA&radius=100&sort=date";
+// const job_search_url = "https://www.indeed.com/jobs?q=software+developer&l=vancouver%2C+WA&radius=100&sort=date";
+const url = "https://www.indeed.com/jobs?q=software+developer&l=Longview,+WA&radius=50&sort=date";
 
 var indeedJobs = [];
-// let jobsTitles = [];
-// let companies = [];
 
 const OUTPUTFILEPATH = "./output/";
 const OUTPUTFILENAME = "data.html";
 
-const afterReadData = (err, data) => {
+const afterGetHtml = (err, data) => {
     if(err) throw err;
-    const html = data.toString();
-    indeedParse.parseIndeedJobs(html, indeedJobs);
-    console.log(indeedJobs);
+    // indeedParse.parseIndeedJobs(data, indeedJobs);
+    // console.log(indeedJobs);
+    console.log(data);
 };
 
+
 const main = () => {
-    fi.readFromFile(OUTPUTFILEPATH+OUTPUTFILENAME, afterReadData);
+    // fi.readFromFile(OUTPUTFILEPATH+OUTPUTFILENAME, afterGetHtml);
+    pptr.getHtml(url, afterGetHtml);
 };
 
 main();
 
-// puppeteer.launch().then(
-//     async browser => {
-//         const page = await browser.newPage();
-//         await page.goto(url);
-//         const content = await page.content();
-//         // fi.writeToFile("./output/data.html", html);
-//         // await $('#results-body', html).each(function() {
-//         //     // console.log(this);
-//         //     // console.log($(this).text());
-//         // });
-//         const resultsBody = await $('.jobtitle', content);
-//         await resultsBody.attribs.array.forEach(element => {
-            
-//         });
-//         await console.log();
-//         // await console.log(resultsBody.attribs);
-//         await browser.close();
-// });
